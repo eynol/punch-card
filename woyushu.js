839e827cfc3b3ee087445cee7ec450b484c3a355
eynol <625608852@qq.com>
2019-08-25 20:01:49 +0800

    woyushu

--- woyushu.js
+++ woyushu.js
@@ -38,7 +38,7 @@ async function fetch(url, options = {}) {
         headers['Upgrade-Insecure-Requests'] = 1
     }
     if (!headers['Referers']) {
-        headers['Referer'] = "http://www.gezhongshu.com/forum.php"
+        headers['Referer'] = "http://www.woyushu.com"
     }
 
     // prepare config cookie
@@ -53,23 +53,24 @@ async function fetch(url, options = {}) {
     // after config cookie
 
     let cookies;
-    let _setCookies1 = res.headers.raw()
+
     let _setCookies = res.headers.get('set-cookie');
     // oooops node-fetch the header
-    const _d = x => x.replace(/^,/, '');
-    _setCookies = _setCookies.replace('; httponly', '').split('path=/').filter(x => !!x.trim())
-        .map((x, index, arr) => arr + 1 == arr.length ? _d(x) : _d(x) + 'path=/')
-    if (_setCookies instanceof Array)
-        cookies = _setCookies.map(Cookie.parse);
-    else
-        cookies = [Cookie.parse(_setCookies)];
+    if (_setCookies) {
+        const _d = x => x.replace(/^,/, '');
+        _setCookies = _setCookies.replace(/;\shttponly/ig, '').split('path=/').filter(x => !!x.trim())
+            .map((x, index, arr) => arr + 1 == arr.length ? _d(x) : _d(x) + 'path=/')
+        if (_setCookies instanceof Array)
+            cookies = _setCookies.map(Cookie.parse);
+        else
+            cookies = [Cookie.parse(_setCookies)];
 
 
-    // console.log(cookies)
-    cookies.forEach(cookie => cookiejar.setCookieSync(cookie, $url.protocol + '//' + $url.host))
+        // console.log(cookies)
+        cookies.forEach(cookie => cookiejar.setCookieSync(cookie, $url.protocol + '//' + $url.host))
 
-    saveCookieFile(cookiejar)
-
+        saveCookieFile(cookiejar)
+    }
     return res;
 }
 
@@ -79,25 +80,13 @@ const $ = require('cheerio');
 
 
 async function checkLogin() {
-    let res = await fetch('http://www.gezhongshu.com/forum.php')
-
-    let text = await res.text();
-    // console.log(text)
-    let testReg = /"访问我的空间"><\/a>/gm
-    if (testReg.test(text)) {
+    let res = await fetch('http://www.woyushu.com')
+    if (res.redirected && res.url.match(/login/)) {
+        log('检测到未登录')
         return false;
     } else {
-
-        let urlReg = /href=\"([^"]*)" onclick="ajaxget\(this\.href,/im;
-        let match = urlReg.exec(text);
-        if (match) {
-            let url = match[1];
-            let res = await fetch('http://www.gezhongshu.com/' + url);
-            let html = await res.json()
-            // console.log(html)
-            log('签到成功')
-        }
-        return Promise.reject('=_=')
+        log('检测到已登录')
+        return true
     }
 }
 
@@ -107,11 +96,7 @@ async function checkLogin() {
 async function getLoginParams() {
     let resp = await fetch('http://www.gezhongshu.com/member.php?mod=logging&action=login&referer=&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login')
     let txt = await resp.text()
-    let loginHashReg = /loginhash=(\w*)"/m;
-    let formHashReg = /name="formhash" value="(\w*)"/
 
-    let loginHashMatch = loginHashReg.exec(txt);
-    let formHashMatch = formHashReg.exec(txt);
     if (loginHashMatch && formHashMatch) {
         return {
             loginhash: loginHashMatch[1],
@@ -120,52 +105,34 @@ async function getLoginParams() {
     }
 }
 
-checkLogin().then(online => {
-    log('online? ', online)
-    if (online) {
-        return daylySign()
-    } else {
-        getLoginParams().then(login).then(daylySign)
-    }
-}).catch(e => {
-    if (e === '=_=') {
-        log('=——=c 签到成功')
-    }else{
-        log(e)
-    }
-})
+
 
 async function getFormHash() {
     let x = let
 }
 
 
-async function login({ formhash = '', loginhash }) {
-    const { username, password } = accounts.gezhongshu;
+async function login() {
+    const { username, password } = accounts.woyushu;
     var data = {
-        formhash: formhash,
-        referer: "http://www.gezhongshu.com/plugin.php?id=k_misign:sign",
-        loginfield: "username",
-        username: username,
+        email: username,
         password: password,
-        questionid: 0,
-        answer: ""
     }
 
 
-    let dataString = Object.keys(data).map(key => key + '=' + data[key]).join('&')
-    
+    let dataString = Object.keys(data).map(key => key + '=' + encodeURIComponent(data[key])).join('&')
+
     let fetchOptions = {
         method: "post",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
-            Referer: "http://www.gezhongshu.com/plugin.php?id=k_misign:sign",
+            Referer: "http://www.woyushu.com/deal/login",
             "Upgrade-Insecure-Requests": 1,
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
         },
         body: dataString,
     }
-    let resp = await fetch(`http://www.gezhongshu.com/member.php?mod=logging&action=login&loginsubmit=yes&handlekey=login&loginhash=${loginhash}&inajax=1`, fetchOptions)
+    let resp = await fetch(`http://www.woyushu.com/deal/login`, fetchOptions)
     let text = await resp.text();
     log('登录成功')
     // console.log(text)
@@ -180,18 +147,34 @@ async function logout() {
 }
 
 async function daylySign() {
-    let res = await fetch('http://www.gezhongshu.com/forum.php');//获取新的页面
+    let res = await fetch('http://www.woyushu.com/User/signin', {
+        headers: {
+            "X-Requested-With": "XMLHttpRequest"
+        }
+    });//获取新的页面
     let html = await res.text();
-
-    let urlReg = /href=\"([^"]*)" onclick="ajaxget\(this\.href,/im;
-    let match = urlReg.exec(html);
-    if (match) {
-        let url = match[1];
-        let res = await fetch('http://www.gezhongshu.com/' + url);
-        let html = await res.json()
-        log('签到成功')
-    } else {
-        log('已经签到了')
-    }
+    log(html)
     //                               plugin.php?id=k_misign:sign&operation=qiandao&format=button&formhash=ed573f62
 }
+
+
+
+checkLogin().then(isLogin => {
+    if (!isLogin) {
+        return login()
+    }
+}).then(daylySign)
+// .then(online => {
+//     log('online? ', online)
+//     if (online) {
+//         return daylySign()
+//     } else {
+//         getLoginParams().then(login).then(daylySign)
+//     }
+// }).catch(e => {
+//     if (e === '=_=') {
+//         log('=——=c 签到成功')
+//     }else{
+//         log(e)
+//     }
+// })
